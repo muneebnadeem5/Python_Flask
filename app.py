347ac0ba0c0905370f913eba2e460f7206f5f039
Muhammad Muneeb <muhammad@LHR-Muhammad-Muneeb-MAC.local>
2022-08-15 19:10:03 +0500

    First Commit to push

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return   "<h1>Welcome to My_Application Index Page! </h1> "
+
+
+@app.route("/Hi!/")
+def who():
+    return   "<h1>Who are you</h1>"
+
+@app.route("/Hi!/<username>")
+def greet(username):
+    return   f"<h1>Hi! There, {username}! </h1>"
